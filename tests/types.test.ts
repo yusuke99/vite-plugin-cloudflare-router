e584ff20a778ab9a6cd7ba12d031d00e1a9d57a5
yusuke <66258931+yusuke99@users.noreply.github.com>
2026-08-21 15:13:46 +0900

    chore: typo

--- tests/types.test.ts
+++ tests/types.test.ts
@@ -2,7 +2,7 @@ import type { DefineHandler } from '../src/runtime.js';
 import { describe, expectTypeOf, test } from 'vite-plus/test';
 import { defineHandler as _defineHandler, json } from '../src/runtime.js';
 
-describe('defineHandler', () => {
+describe('DefineHandler', () => {
   test('infers params', () => {
     const defineHandler = _defineHandler as DefineHandler<{ message: string }>;
     const GET = defineHandler(({ params }) => json({ message: params.message }));
@@ -11,7 +11,7 @@ describe('defineHandler', () => {
     expectTypeOf<Payload>().toEqualTypeOf<{ message: string }>();
   });
 
-  test('inters catch-all params', () => {
+  test('infers catch-all params', () => {
     const defineHandler = _defineHandler as DefineHandler<{ catchall: string[] }>;
     const GET = defineHandler(({ params }) => json({ message: params.catchall.join(',') }));
 
